f2ba821c4a7899b9dbcbcd7d0b90d26a572fba0e
yogeshmcs <yogesh.mcs17.du@gmail.com>
2018-04-19 13:01:35 +0530

    adding notifications functionality

--- Spent/popup.js
+++ Spent/popup.js
@@ -1,14 +1,12 @@
 $(function(){
-	
-	 chrome.storage.sync.get(['total','limit'],function(budget){
+
+    chrome.storage.sync.get(['total','limit'],function(budget){
         $('#total').text(budget.total);
         $('#limit').text(budget.limit);
-        
-        });
+    });
 
-
-	 $('#spendAmount').click(function(){
-        chrome.storage.sync.get('total',function(budget){
+    $('#spendAmount').click(function(){
+        chrome.storage.sync.get(['total', 'limit'],function(budget){
             var newTotal = 0;
             if (budget.total){
                 newTotal += parseInt(budget.total);
@@ -19,12 +17,23 @@ $(function(){
                 newTotal += parseInt(amount);
             }
 
-	   chrome.storage.sync.set({'total':newTotal});
+            chrome.storage.sync.set({'total': newTotal}, function(){               
+                if (amount && newTotal >= budget.limit){
+                    var notifOptions = {
+                        type: "basic",
+                        iconUrl: "icon48.png",
+                        title: "Limit reached!",
+                        message: "Uh oh, look's like you've reached your alloted limit."
+                };
+                chrome.notifications.create('limitNotif', notifOptions);
 
-	   $('#total').text(newTotal);
-	   $('#amount').val('');
-    
-          });
-       });
-      });
-    
+            }
+            });
+            $('#total').text(newTotal);
+            $('#amount').val('');
+
+           
+
+        });
+    });
+});
